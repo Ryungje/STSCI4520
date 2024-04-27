098d4399602213b5488002cd691d3dff0caa40ae
James Chun <jrc447@cornell.edu>
2024-04-27 15:20:37 -0400

    Updates

--- create_dataset.R
+++ create_dataset.R
@@ -1,7 +1,7 @@
 
 
 
-data <- scan("../Project Dataset/CRND0103-202404080750/2000/CRND0103-2000-NC_Asheville_8_SSW.txt", what="character")
+data <- scan("../Project Dataset/CRND0103-202404080750/2011/CRND0103-2011-AK_Sitka_1_NE.txt", what="character")
 headers <- scan("../Project Dataset/headers.txt", what="character")
 headers <- headers[29:56]
 
