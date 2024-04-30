c82f395fcb8d31fb81b04c2f0d21464dcca6dd8e
James Chun <jrc447@cornell.edu>
2024-04-30 00:20:06 -0400

    Close to extracting data and making dataframe

--- create_dataset.R
+++ create_dataset.R
@@ -1,29 +1,47 @@
 
 
-for (i in 2000:2024){
+# Scan headers for col names
+headers <- scan("headers.txt", what="character")
+headers <- headers[29:56] #extract only relevant headers
+
+# Initialize data
+data <- matrix(ncol=30)
+
+for (i in 2000:2000){
   # Get path for each folder year
-  year_path <- file.path("CRND0103-202404080750", as.character(2000)) 
-  #UPDATE YEAR USING LOOP
+  year_path <- file.path("CRND0103-202404080750", as.character(i))
   
   # Obtain .txt files in folder
   files <- list.files(year_path, full.names = TRUE, pattern = "\\.txt$") 
   
-  # Scan data and headers
-  data <- scan(files[1], what="character")
-  headers <- scan("headers.txt", what="character")
   
-  headers <- headers[29:56] #extract only relevant headers
-  
-  # Create dataframe, while converting data into numerics
-  data <- data.frame(matrix( as.numeric(unlist(data) ), ncol=length(headers), byrow = TRUE))
-  names(data) <- headers
-  
-  data <- data[-c(12:28)] # remove unnecessary columns
+  # Scan data in folder
+  for (f in files){
+    temp <- scan(f, what="character")
+    
+    # Extract file_name, aka station name, and state data
+    file_name <- str_extract(basename(f), "(?<=-)[A-Z]+_[A-Za-z_]+\\d*_\\w+")
+    state <- str_extract(file_name, "^[A-Z]+")
+    
+    # Collect data into matrix
+    temp <- matrix(temp, byrow=TRUE, ncol=28)
+    temp <- cbind(temp, file_name)
+    temp <- cbind(temp, state)
+    
+    
+    # Append temp to data
+    if ( is.na(data[1]) ){
+      data <- temp
+    }else {
+      data <- rbind(data, temp)
+    }
+    
+  }
   
 }
 
 
-
-head(data)
-
+data <- data.frame(data)
+names(data) <- c(headers, "Station_Name", "State")
+View(data)
 
