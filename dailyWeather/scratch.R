6014ff8aee325297dd559c81c2b8a6f6b3b7d3b8
James Chun <jrc447@cornell.edu>
2024-05-06 15:54:36 -0400

    Completed testing for get_temp_trend

--- dailyWeather/scratch.R
+++ dailyWeather/scratch.R
@@ -4,7 +4,7 @@ library("devtools")
 data <- dailyWeather::data
 
 dev <- function(){
-  devtools::load_all() #to load in package
+  #devtools::load_all() #to load in package
   #devtools::document() #to load in function documentation
   devtools::test() #run tests
   #devtools::run_examples() #run examples in documentations
--- dailyWeather/tests/testthat/test_get_temp_trend.R
+++ dailyWeather/tests/testthat/test_get_temp_trend.R
@@ -1,11 +1,23 @@
 
 test_that( "time series", {
 
-  #data <- dailyWeather::data
+  data <- dailyWeather::data
 
+  # basic functionality of the function
+  test1 <- get_temp_trend(data, "NC_Asheville_13_S")
+  expect_equal(
+    nrow(test1),
+    25
+  )
+  expect_true(all(test1$Year == 2000:2024))
+  expect_true(is.numeric(test1$Temp))
 
+  test2 <- test1
+  expect_equal(
+    test2$Temp[1],
+    0
+  )
 
-  #expect_equal(   )
 
 })
 
